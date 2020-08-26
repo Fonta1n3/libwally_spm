e34a112659c59cef9d134dc2f957125b9a21f970
Fonta1n3 <fontainedenton@googlemail.com>
2020-08-26 17:09:26 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "libwally-spm",
-            dependencies: ["libwally"]),
+            dependencies: []),
         .testTarget(
             name: "libwally-spmTests",
             dependencies: ["libwally-spm"]),
